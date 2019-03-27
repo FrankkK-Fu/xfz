3c208d7f6adac83e54401e436700d23f65c82e92
frankkk fu <875826241@qq.com>
2019-03-28 03:10:01 +0800

    修改了setting

--- xfz/settings.py
+++ xfz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kedsopkmi@#8=80l2c$3m6%=j0@vgjdlrx6ez2#a$3iqtxjin#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','132.232.67.9']
 
 
 # Application definition
